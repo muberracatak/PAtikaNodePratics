f7364410855cfca4067766be17e890180384f5e1
Müberra <55498680+muberracatak@users.noreply.github.com>
2022-04-19 02:56:43 +0300

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,15 @@
+function circleArea (yaricap){
+    let pi = 3.14;
+    console.log(pi * yaricap * yaricap);
+
+}
+function circleCircumference (yaricap){
+    let pi = 3.14;
+    console.log(2*pi*yaricap);
+
+}
+
+module.exports={
+    circleArea,
+    circleCircumference
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+
+const circleArea = require('./circle');
+circleArea.circleArea(5);
+circleArea.circleCircumference(5);
